e5eac3b7d6b561a8c8de18d6c0b5ae83499ec4e3
Alisa Herr <alisa@unitymakes.us>
2020-04-06 14:59:17 -0400

    Update GTM ID

--- wp-content/themes/unity-core/resources/views/partials/head.blade.php
+++ wp-content/themes/unity-core/resources/views/partials/head.blade.php
@@ -5,7 +5,7 @@
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-MN7WQ2R');</script>
+    })(window,document,'script','dataLayer','GTM-NWRZS3T');</script>
     <!-- End Google Tag Manager -->
   @endif
   <meta charset="utf-8">
