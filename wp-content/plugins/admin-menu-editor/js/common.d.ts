e0bf828c00fcf7821a7035c6ebda9a7f1ba0d8f7
Andy Marino <andy@unitywebagency.com>
2020-04-08 13:56:40 -0400

    remove inactive and unnecessary plugins

--- wp-content/plugins/admin-menu-editor/js/common.d.ts
+++
@@ -1,6 +0,0 @@
-interface AmeDictionary<T> {
-	[mapKey: string] : T;
-}
-
-// noinspection JSUnusedGlobalSymbols
-type KeysMatchingType<T, V> = { [K in keyof T]: T[K] extends V ? K : never }[keyof T];
